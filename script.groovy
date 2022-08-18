7c5023bce06dd9649cd279d652082c7da8bf7f80
Emmanuelxsx <66085707+Emmanuelxsx@users.noreply.github.com>
2022-08-18 07:20:15 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,4 +3,15 @@ echo 'building the application'
 
 }
 
+def testApp() {
+echo 'testing the application'
+  
+}
+
+def deployApp() {
+echo 'deploying the application'
+echo "deploying version ${params.VERSION}"
+  
+}
+
 return this
